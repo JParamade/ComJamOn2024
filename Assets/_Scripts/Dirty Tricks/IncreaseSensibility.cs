56ff31f993a4fce9adbd72ad14025ef165c8ead1
SignoraTetona <71043659+SignoraTetona@users.noreply.github.com>
2024-03-17 22:45:13 +0100

    sensibilityIncrease

--- Assets/_Scripts/Dirty Tricks/IncreaseSensibility.cs
+++ Assets/_Scripts/Dirty Tricks/IncreaseSensibility.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class IncreaseSensibility : PutadaBase
 {
-    public MouseManager mouse;
+    public float duration = 1f;
+    private float totalDuration;
 
+
+
+    private void Awake()
+    {
+        totalDuration = duration;
+
+    }
     public override void Efecto()
     {
-        mouse.increaseSensitivity = true;
+        if (duration > 0)
+        {
+            duration -= Time.deltaTime;
+        }else
+        {
+            duration = totalDuration;
+            this.GetComponent<IncreaseSensibility>().enabled = false;
+        }
     }
-    public override void EliminarEfecto()
+    private void OnEnable()
     {
-        mouse.increaseSensitivity = false;
+        MouseManager.Instance.increaseSensitivity = true;
+    }
+    private void OnDisable()
+    {
+        MouseManager.Instance.increaseSensitivity = false;
     }
 
 }
